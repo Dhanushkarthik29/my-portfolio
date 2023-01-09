77bc82d1bb2855dd8286055a5722cec98193ce17
Dhanush Krishna <dhanush549.d@gmail.com>
2023-01-09 15:47:22 +0000

    portfolio v1

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+include 'libs/load.php' 
+?>
+
+<?php load_templates('main');?>
---
+++ libs/load.php
@@ -0,0 +1,5 @@
+<?php 
+    function load_templates($name){
+        include $_SERVER['DOCUMENT_ROOT']."/templates/$name.php";
+    }
+?>
